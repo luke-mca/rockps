e7414af9c3f1e1ebfb39f3de37255c40e43e0978
luke-mca <mcluke2004@gmail.com>
2023-06-27 22:02:32 -0500

    Add basic UI

--- script.js
+++ script.js
@@ -12,42 +12,47 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let playerSelection = prompt("Rock, Paper, or Scissors? ");
-    return playerSelection;
-}
 
 function playRound(player, comp) {
     if (player == "Rock" || player == "rock" || player == "ROCK") {
         if (comp == "Rock") {
+            ties += 1;
             return "Tie game! Rock vs Rock.";
         }
         if (comp == "Paper") {
+            losses += 1;
             return "You lose, Paper beats Rock.";
         }
         if (comp == "Scissors") {
+            wins += 1;
             return "You win, Rock beats Scissors.";
         }
     }
     if (player == "Paper" || player == "paper" || player == "PAPER") {
         if (comp == "Paper") {
+            ties += 1;
             return "Tie game! Paper vs Paper.";
         }
         if (comp == "Rock") {
+            wins += 1;
             return "You win, Paper beats Rock.";
         }
         if (comp == "Scissors") {
+            losses += 1;
             return "You lose, Scissors beats Paper.";
         }
     }
     if (player == "Scissors" || player == "scissors" || player == "SCISSORS") {
         if (comp == "Scissors") {
+            ties += 1;
             return "Tie game! Scissors vs Scissors.";
         }
         if (comp == "Rock") {
+            losses += 1;
             return "You lose, Rock beats Scissors.";
         }
         if (comp == "Paper") {
+            wins += 1;
             return "You win, Scissors beats Paper.";
         }
     }
@@ -58,8 +63,4 @@ function game() {
     let playerSelection = getPlayerChoice();
     let computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
-}
-
-for (let i = 0; i<5; i++) {
-    game();
 }
